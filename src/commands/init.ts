a224b66d2d202b5972b009988b5776e7e172589a
ReeseHatfield <hatfield.69@wright.edu>
2024-02-01 16:27:57 -0500

    finalize init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -15,7 +15,47 @@ const init = (args: string[]) => {
 
     mkdirSync(dirName, { recursive: true });
 
+    const directories = ["lib", "src", "docs", "out", "build", "assets"];
+
+    directories.forEach((subDir) => {
+        mkdirSync(dirName + "/" + subDir, {recursive: true});
+    })
+
+    const buildKettleContent = {
+        "libPath": "./lib",
+        "srcPath": "./src",
+        "docsPath": "./docs",
+        "outPath": "./out",
+        "buildPath": "./build",
+        "assetsPath": "./assets",
+        "entryPath": "./src/Driver.java"
+    }
+
+
+    writeFile(dirName + "/build.kettle", JSON.stringify(buildKettleContent, null, 2), err => {
+        if(err){
+            console.log("Something went wrong while creating build.kettle");
+        }
+    })
+
+    const helloWorld = 
+
+`
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
     
+}
+`
+
+    writeFile(dirName + "/src/Driver.java", helloWorld, err => {
+        if(err){
+            console.log("Something went wrong creating the starting java file");
+        }
+    });
+
 }
 
 export default init;
