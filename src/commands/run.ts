446052e36401556775608a1e6e62c54fc4d2b88f
ReeseHatfield <hatfield.69@wright.edu>
2024-02-02 08:32:36 -0500

    run cmd now reads from build.kettle

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,12 +1,15 @@
 import { spawn } from 'child_process';
+import { readFile, readFileSync } from 'fs';
 import glob from 'glob';
 
-const run = () => {
+const run = (args: string[]) => {
+
+    const buildKettle: JSON = getBuildKettle();
 
     // use glob to stich all the files together, cannot handle * without it
-    const javaFiles = glob.sync('./src/*.java');
+    const javaFiles = glob.sync(`${buildKettle["srcPath"]}/*.java`);
 
-    const compileJava = spawn('javac', ['-d', 'out', ...javaFiles]);
+    const compileJava = spawn('javac', ['-d', `${buildKettle["outPath"]}`, ...javaFiles]);
 
     compileJava.stderr.on('data', (data) => {
         console.error(`Error from javac: ${data}`);
@@ -19,9 +22,13 @@ const run = () => {
             return;
         }
 
-        const runJava = spawn('java', ['-cp', 'out:lib/*', 'Driver']);
+        // Windows uses a different compile syntax than *nix
+        const libChar = (process.platform === "win32") ? ";" : ":";
+
+        const runJava = spawn('java', ['-cp', `${buildKettle["outPath"]}${libChar}${buildKettle["libPath"]}/*`, 'Driver', ...args]);
 
         runJava.stdout.pipe(process.stdout);
+        runJava.stderr.pipe(process.stderr);
         process.stdin.pipe(runJava.stdin);
 
         runJava.on('exit', (code) => {
@@ -36,5 +43,21 @@ const run = () => {
         console.error('Error spawning javac:', err);
     });
 };
+
+
+function getBuildKettle(): JSON {
+
+    let data: JSON;
+    //" ./ " => relative to execution path
+    try{
+        data = JSON.parse(readFileSync('./build.kettle', { encoding: 'utf8', flag: 'r' }));
+    }
+    catch(err: any){
+        console.error("Fatal error: could not read build.kettle");
+        process.exit(1);
+    }
+
+    return data;
+}
 
 export default run;
